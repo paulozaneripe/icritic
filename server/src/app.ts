0bffaec4bd5344a1c9a55e7b4f54b000a21baa45
Paulo Zaneripe <paulozaneripe@hotmail.com>
2023-02-13 15:14:05 -0300

    chore: initial swagger setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import path from 'path';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
+
+const swaggerFile = yaml.load(path.resolve(__dirname, './swagger.yaml'));
 import { routes } from '@src/routes';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
 
 export { app };
